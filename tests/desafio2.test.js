ba734624a9994fa7a4076920d84d4b59ecd4229a
Eduardo Hurtado <eduardohug@outlook.com>
2024-11-07 12:41:32 -0600

    Agregar pruebas automatizadas para navegación usando Puppeteer con métodos de texto, XPath y CSS

--- tests/desafio2.test.js
+++ tests/desafio2.test.js
@@ -2,33 +2,79 @@ const puppeteer = require('puppeteer')
 
 describe('Prueba de navegación Desafio Sesion 2', () => {
 
-    it('Desafio', async () => {
+
+       it('Desafio Texto', async () => {
 
         const browser = await puppeteer.launch({ headless: false })
         const page = await browser.newPage()
         await page.setViewport({ width: 1366, height: 768 });
         await page.goto('https://store.steampowered.com')
 
-        const [elementHandle] = await page.$x('//a[contains(@class, "menuitem") and contains(text(), "Acerca de")]');
+        const linkText = 'Acerca de'
+
+        await page.evaluate((linkText) => {
+
+            //Busca todos los elementos a de la página y los convierte en un array
+            const elements = Array.from(document.querySelectorAll('a'));
+            //Busca el elemento que contenga el texto que se le pasa por parámetro
+            const targetElement = elements.find(element => element.textContent.trim() === linkText);
+            if (targetElement) {
+                targetElement.click();
+            }
+        }, linkText);
+
+       // await browser.close()
+
+    });
 
 
-        if (elementHandle) {
-            const text = await page.evaluate(element => element.textContent, elementHandle);
-            console.log('Elemento encontrado:', text.trim());
-            await elementHandle.click();
-        } else {
-            console.log('Elemento no encontrado');
-        }
+    it('Desafio Xpath', async () => {
 
+        const browser = await puppeteer.launch({ headless: false })
+        const page = await browser.newPage()
+        await page.setViewport({ width: 1366, height: 768 });
+        await page.goto('https://store.steampowered.com')
 
+        const xp = '::-p-xpath(//*[@id="global_header"]/div/div[2]/a[3])'
 
+        await page.waitForSelector(xp)
 
+        const element = await page.$(xp)
 
+        console.log({element})
 
+        element.click()
 
+       // await browser.close()
+            
 
 
     });
+
+    it('Desafio CSS', async () => {
+
+        const browser = await puppeteer.launch({ headless: false })
+        const page = await browser.newPage()
+        await page.setViewport({ width: 1366, height: 768 });
+        await page.goto('https://store.steampowered.com')
+
+        const cssSelector = '#global_header > div > div.supernav_container > a:nth-child(5)'
+
+        await page.waitForSelector(cssSelector)
+
+        const element = await page.$(cssSelector)
+
+        console.log({element})
+
+        element.click()
+
+        await browser.close() 
+
+        // await browser.close()
+
+    });
+
+ 
 
 })
 
